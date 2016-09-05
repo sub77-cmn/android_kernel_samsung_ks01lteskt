6604c2a751a45e0a94e21fd783d4f2d6e5df0d3a
sub77 <sub77@ymail.com>
2016-09-05 17:55:06 +0200

    Update Build scripts

--- test_kernel_matisse.sh
+++ test_kernel_matisse.sh
@@ -2,10 +2,11 @@
 
 export ARCH=arm
 export CROSS_COMPILE=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-
+#rm -rf output
+#make clean
 mkdir output
 
-make -C $(pwd) O=output cyanogenmod_matissewifi_defconfig
+make -C $(pwd) O=output test_matissewifi_defconfig
 make -j64 -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
